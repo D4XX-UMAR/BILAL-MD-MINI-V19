c4b43da664552f2e10fa5d078668ad85cef2c775
UMAR • FAROOQ <ufprince1@gmail.com>
2026-05-25 11:00:34 +0500

    Update ytdl-play.js

--- plugins/ytdl-play.js
+++ plugins/ytdl-play.js
@@ -26,7 +26,7 @@ cmd({
         }
 
         // API call (replace APIKEY with your valid key)
-        const apiUrl = `https://gtech-api-xtp1.onrender.com/api/audio/yt?apikey=APIKEY&url=${encodeURIComponent(videoUrl)}`;
+        const apiUrl = `https://api.ilhm.my.id/download/youtube?url=${encodeURIComponent(videoUrl)}`;
         const response = await fetch(apiUrl);
         const data = await response.json();
 
